d90fac8051d81a3eb3f7f24143bb53db951b9a73
chetanbuzzfe <139129267+chetanxx@users.noreply.github.com>
2023-07-17 14:21:07 +0530

    First Commit

---
+++ src/main/java/bard/Openai.java
@@ -0,0 +1,21 @@
+package bard;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Openai {
+
+	public static void main(String[] args) {
+
+
+		FileInputStream fileinputstream = new FileInputStream();
+		XSSFWorkbook book = new XSSFWorkbook();
+		
+
+	}
+}
